c64a2b025196dbba2fb546c16320abf7f80b6ae8
andrzej-herman <aherman@san.edu.pl>
2022-11-12 16:46:08 +0100

    12.11.2022 #2

--- SanFilmoteka/Program.cs
+++ SanFilmoteka/Program.cs
@@ -8,7 +8,7 @@ var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
 builder.Services.AddRazorPages();
 builder.Services.AddServerSideBlazor();
-//builder.Services.AddSingleton<IMovieService, MovieService>();
+builder.Services.AddSingleton<IMovieService, MovieService>();
 
 var app = builder.Build();
 
